55b6beb9e20654941894c07b41475bac4ffcba93
arisstogiannos <arsenis_sto@yahoo.gr>
2023-05-17 22:19:45 +0300

    third

--- travelpack/src/travelpack/Hotel.java
+++ travelpack/src/travelpack/Hotel.java
@@ -1,8 +1,19 @@
 package travelpack;
 
+import java.util.ArrayList;
+
 public class Hotel {
 	private String name;
 	private String city;
 	private int price;
 	private int code;
+	ArrayList<Room> rooms = new ArrayList<>();
+	
+	
+	void createRoomList() {
+		
+		
+	}
 }
+
+
--- travelpack/src/travelpack/Room.java
+++ travelpack/src/travelpack/Room.java
@@ -1,7 +1,20 @@
 package travelpack;
 
 public class Room {
+	private int code;
 	private String type;
 	private int capacity;
-	private boolean[][] available;
+	private boolean[][] available = new boolean[12][31];
+	
+	public Room(String type, int capacity, boolean[][] available, int code) {
+		this.code=code;
+		this.type = type;
+		this.capacity = capacity;
+		for(int i = 0; i<12;i++) {
+			for(int j = 0;i<31;j++) {
+				available[i][j] = true;
+			}
+		}
+	}
+	
 }
