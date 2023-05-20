0347593896fe8e32e922cc79578991b5420594b3
arsen <arsen@192.168.1.4>
2023-05-20 21:35:13 +0300

    12

--- travelpack/src/travelpack/Frame3.java
+++ travelpack/src/travelpack/Frame3.java
@@ -13,25 +13,7 @@ public class Frame3 extends JFrame {
 
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					Frame3 frame = new Frame3();
-//					frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
-
-	/**
-	 * Create the frame.
-	 */
+	
 	public Frame3() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
